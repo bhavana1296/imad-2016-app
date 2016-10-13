e05f17ad3100693a3e2a02f0ee1212032c3787d5
bhavana1296 <bhavana.chindu@gmail.com>
2016-10-13 12:56:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles=
+{
+    'article-one':{Title:'Article One|Bhavana', Heading:'Article-one',Content:'This is article one'},
+    'article-two':{Title:'Article Two|Bhavana', Heading:'Article-two',Content:'This is article two'},
+    'article-three':{Title:'Article Three|Bhavana', Heading:'Article-three',Content:'This is article three'},
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,17 +19,32 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one',function(req,res){
-   res.send('This is article one'); 
+app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
+   res.send(createTemplate(articles[articleName])); 
 });
 
-app.get('/article-two',function(req,res){
-   res.send('This is article two'); 
-});
+function createTemplate(data){
+    Title=data.Title;
+    Heading=data.Heading;
+    Content=data.Content;
+    var htmlTemplate=
+        `<html>
+        <head>
+            <title>${Title}</title>
+            <link href="/ui/style.css" rel="stylesheet"/>
+        </head>
+        <body>
+            <div class="container">
+                <h1>${Heading}</h1>
+                </hr>
+                <p>${Content}</p>
+            </div>
+        </body>
+        </html>`;
+        return htmlTemplate;
+}
 
-app.get('/article-three',function(req,res){
-   res.send('This is article three'); 
-});
 
 app.get('/ui/cat.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'cat.png'));
