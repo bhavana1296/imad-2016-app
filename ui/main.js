b86822511baf65573f80ae5e5869be76c6ff32e9
bhavana1296 <bhavana.chindu@gmail.com>
2016-10-13 23:14:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ button.onclick = function(){
     
     request.onreadystatechange=function(){
         if(request.readystate==XMLHttpRequest.DONE){
+            console.log("Inside DONE");
             if(request.status===200){
                 var counter=request.responseText;
                 var span = document.getElementById('count');
