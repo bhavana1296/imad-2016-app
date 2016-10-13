ac436a7ac856577231a0277ab936ed160e02d55e
bhavana1296 <bhavana.chindu@gmail.com>
2016-10-13 18:19:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ button.onclick = function(){
     var request = new XMLHttpRequest();
     
     request.onreadystatechange=function(){
-        if(request.readystate=XMLHttpRequest.DONE){
+        if(request.readystate==XMLHttpRequest.DONE){
             if(request.status==200){
                 var counter=request.responseText;
                 var span = document.getElementById('count');
@@ -16,7 +16,7 @@ button.onclick = function(){
         }
     }
     
-    request.open('GET','bhavana1296.imad.hasura.io/counter',true);
+    request.open('GET','https://bhavana1296.imad.hasura.io/counter',true);
     request.send(null);
 
 };
