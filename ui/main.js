6207b897c012546f210e9b7c596e3c118c6a1b98
bhavana1296 <bhavana.chindu@gmail.com>
2016-10-13 15:27:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,8 @@ var move=0;
 function MoveLeft(){
     move=move+5;
     img.style.marginLeft=move+'px';
+    if(move>=50)
+        move=0;
 }
 img.onclick=function(){
     var interval = setInterval(MoveLeft,50);
