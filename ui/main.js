e438297070f69bff3682e7da6f3c07cf43117a35
bhavana1296 <bhavana.chindu@gmail.com>
2016-10-13 15:09:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change value
-var text= document.getElementById('heading');
-text.innerHTML='Bhavana';
+var element= document.getElementById('heading');
+element.innerHTML='Bhavana';
