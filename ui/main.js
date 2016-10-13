60f64cd99a7c167c50e681869b469b0b06c6df1a
bhavana1296 <bhavana.chindu@gmail.com>
2016-10-13 15:25:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,13 @@ console.log('Loaded!');
 //change value
 var element= document.getElementById('heading');
 element.innerHTML='Bhavana';
+//move image
+var image=document.getElementById('img');
+var move=0;
+function MoveLeft(){
+    move=move+5;
+    img.style.marginLeft=move;
+}
+img.onclick=function(){
+    var interval = setInterval(MoveLeft,50);
+};
